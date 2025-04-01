db0377d2a76e82cb1ba09cc43e0b5db3199885e6
Nitik-sharma <sharmanitik36@gmail.com>
2025-04-01 23:55:41 +0530

    satup backend

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import authRoute from './routes/authRoute.js'
+import dotenv from 'dotenv';
+import { connectDB } from './lib/db.js';
+
+const app = express();
+
+dotenv.config();
+console.log("Prt",process.env.PORT)
+const PORT = process.env.PORT || 5001;
+
+
+
+app.use("/api/auth", authRoute);
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+   connectDB()
+})
