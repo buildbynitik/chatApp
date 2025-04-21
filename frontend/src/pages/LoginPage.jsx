f4dea02365c88a50b74c5adfd9283b32ef3f4952
Nitik-sharma <sharmanitik36@gmail.com>
2025-04-21 21:07:37 +0530

    add some

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -120,7 +120,7 @@ function LoginPage() {
                   Loading.....
                 </>
               ) : (
-                "Create Account"
+                ""
               )}
               Login
             </button>
